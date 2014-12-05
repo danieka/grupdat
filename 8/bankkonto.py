3101c26c8853908a137753f0b18ee6993bded7ca
danieka <danieka@kth.se>
2014-12-05 13:14:33 +0100

    Commented on övning 8

--- 8/bankkonto.py
+++ 8/bankkonto.py
@@ -1,46 +1,70 @@
+# Feel free to extend this program as an exercise. Some suggestions:
+# Implement transfering money between accounts
+# Implement a class Owner that can have several Accounts and every Account has an Owner
+# Implement some checks to make sure the user enters a valid account
 
+class Account:
+	""" A simple class representing an account. 
 
-
-
-
-
-
-
-class Konto:
+	Implements withdraw, balance and insert. Notice that all methods have the self parameter. 
+	But this self parameter is automatically added by Python when calling."""
 	def __init__(self, owner, balance):
-		self.owner = owner
-		self.balance = balance
+		""" The __init__ method is the objects constructor, which is called when the object is created.
+		In this method we do all the setup that needs to be done. In this case we initialize the account
+		by assigning it to an owner and setting the initial balance. """
+		self.owner = owner # Here we set the attribute self.owner to the parameter owner
+		self.bal = balance #The keyword self refers to the own object. So when we write self.bal we mean "set my balance"
 
 	def __repr__(self):
+		"""__repr__ is a special method that is used by Python when we want to print this object.
+
+		So when we write print(ett_konto) this method will be called. The underscores in front of and after repr
+		indicates that this method is a "magic" object. That means that it is used internally by Python.
+		We should never use double underscores for our own methods."""
 		return "Kontot ägs av: " + self.owner + " och hen har: " + str(self.balance) + "kr"
 
 	def withdraw(self, amount):
-		if self.balance >= amount:
-			self.balance -= amount
+		""" This methods withdraws amount from the account. Returns true if withdraw succeds, else returns false.
+		If withdraw fails no money is withdraw.
+
+		The method will fail if the amount is bigger than the balance on the account or if the amount is negative."""
+		if self.bal >= amount and self.bal => 0:  	# We need to check that the client has sufficient money and that the sum to be withdrawn
+			self.bal -= amount						# is positive
 			return True
 		else:
 			return False
 
-	def add(self, amount):
-		self.balance += amount
-		return True
+	def insert(self, amount):
+		""" This method inserts amount to account. Will fail if amount is negative."""
+		if self.bal >= 0:
+			self.bal += amount
+			return True
+		else:
+			return False
 
-konton = {}
+	def balance(self):
+		"""Simply return the balance on the account"""
+		return self.bal
+
+
+accounts = {} 	#For easy lookup we save the accounts in a dictionary
 while True:
 	operation = int(input("1 för nytt konto, 2 för sätta in, 3 för ta ut, 4 för utskrift:"))
 	if operation == 1:
 		name = input("Namn: ")
 		amount = int(input("Balans:"))
-		konton[name] = Konto(name, amount)
-	if operation == 3:
+		accounts[name] = Account(name, amount)
+	else if operation == 2:
 		account = input("Vilket konto? ")
-		amount = int(input("Summa ta ut:"))
-		konton[account].withdraw(amount)
-	print(konton)
-
-#daniels_konto = Konto("Daniel", 100)
-#print(daniels_konto)
-#daniels_konto.withdraw()
+		amount = int(input("Summa att sätta in: "))
+		accounts[account].insert(amount)		# This operation will fail if the account does not exist
+	else if operation == 3:
+		account = input("Vilket konto? ")       # account and Account names do not clash because Python is case-sensitive
+		amount = int(input("Summa ta ut: "))
+		accounts[account].withdraw(amount)
+	else if operation == 4:
+		account = input("Vilket konto? ")
+		print(accounts[account])
 
 
 
--- 8/rational.py
+++ 8/rational.py
@@ -1,20 +1,41 @@
+# Feel free to extend this program as an exercise. Some suggestions:
+# Change __eq__ so it's mathematically correct. Right now 5/5 != 2/2, which is bad.
+# Add some more arithmetic operators. More on what the operatiors should be name can be found on: https://docs.python.org/2/library/operator.html
 
+class Rational:
+	""" A simple class representing a rational number. 
 
-
-
-
-class Rational(object):
+	Only supports adding and eqaulity checking."""
 	def __init__(self, numerator, denominator):
-		self.numerator = int(numerator)
-		self.denominator = int(denominator)
+		self.num = numerator
+		self.den = denominator
 
 	def __repr__(self):
-		return "%d/%d" % (self.numerator, self.denominator)
+		return "%d/%d" % (self.num, self.den) 	# This stuff is called string formatting, and it's a bit more fancy so only pay attention
+												# if you want to.
 
-	def __add__(self, b):
-		return Rational(self.numerator*b.denominator + b.numerator*self.denominator, self.denominator*self.denominator)
+	def __add__(self, other):
+		"""This method adds either two Rationals or one Rational and one Int"""
+		if type(other) == int:
+			return Rational(self.num + other*self.den, self.den)
+		else:
+			return Rational(self.num*other.den + other.num*self.den, self.den*other.den)
 
+	def __eq__(self, other):
+		""" Compares the two rational numbers and returns true if equal. """
+		return self.num == other.num and self.den == other.den
 
-r = [Rational(2, 5), Rational(2,5)]
+a = [Rational(2, 5), Rational(3,5), Rational(4,5)]
+print(Rational(2, 5) + Rational(3,5))
+print(Rational(2, 5).__add__(Rational(3,5)))
 
-print(r[0] + r[1])
+a = Rational(2, 5)
+b = Rational(2, 5)
+a == b #This will return True since both a and b have the value 2/5
+a is b #This will return False since a and b are different objects
+
+d = Rational(2, 5)
+c = d
+d is c #Will return true since they are the same object
+c.num = 5
+print(d.num) #This will print 5, when changing c d was also changed since they are the same object.
